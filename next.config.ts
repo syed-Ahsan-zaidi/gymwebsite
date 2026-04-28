6bca0ac09690ded124c62d6d358bd61886dfef8a
Ali <ahsanzaidi51272@gmail.com>
2026-04-28 23:50:51 +0500

    update prisma schema and database

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, 
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
   // Yeh line Next.js ko batayegi ke build ke waqt DB connection ki fikar na kare
   staticPageGenerationTimeout: 1000, 
 };
