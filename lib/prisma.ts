51d526dc62711e9b752d212e0f6e783a8c35b711
Ali <ahsanzaidi51272@gmail.com>
2026-04-29 13:39:16 +0500

    Fix prisma default export

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
  prisma: PrismaClient
 }
 
-export const prisma =
- globalForPrisma.prisma ||
- new PrismaClient()
+const prisma =
+ globalForPrisma.prisma || new PrismaClient()
 
-if(process.env.NODE_ENV !== 'production')
+if (process.env.NODE_ENV !== "production") {
  globalForPrisma.prisma = prisma
+}
+
+export default prisma
